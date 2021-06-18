225630e4c2b16631bb4fc75e26276d3fb0263d1d
tatsuya yokokura <yokokura.tatsuya@gmail.com>
2021-06-19 07:52:07 +0900

    add: get users data from sampleAPI

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+    <v-btn @click="getUsers">getUsers(data)</v-btn>
+    <v-btn @click="getAllUsers">getUsers(all)</v-btn>
+  </v-container>
+</template>
+
+<script>
+export default {
+  methods: {
+    async getUsers() {
+      const users = await this.$axios.$get(
+        'https://jsonplaceholder.typicode.com/users'
+      )
+      // eslint-disable-next-line no-console
+      console.log(users)
+    },
+    async getAllUsers() {
+      const users = await this.$axios.get(
+        'https://jsonplaceholder.typicode.com/users'
+      )
+      // eslint-disable-next-line no-console
+      console.log(users)
+    },
+  },
+}
+</script>
+
+<style></style>
